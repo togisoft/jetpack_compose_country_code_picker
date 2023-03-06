eee3b4d1449d4749b2b8810e9b3dd57c12665c6f
Harold Martin <harold.martin@gmail.com>
2023-03-06 08:30:07 -0800

    dialog simplification and styling

--- ccp/src/main/java/com/togitech/ccp/component/CountryDialog.kt
+++ ccp/src/main/java/com/togitech/ccp/component/CountryDialog.kt
@@ -11,33 +11,38 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,82 +56,103 @@ import com.togitech.ccp.utils.searchCountry
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
+private val DEFAULT_ROUNDING = 10.dp
+private val DEFAULT_ROW_PADDING = 16.dp
+private val DEFAULT_ROW_FONT_SIZE = 16.sp
+
 @Composable
 fun CountryDialog(
+    dialogClipShape: Shape = RoundedCornerShape(DEFAULT_ROUNDING),
+    rowPadding: Dp = DEFAULT_ROW_PADDING,
+    rowFontSize: TextUnit = DEFAULT_ROW_FONT_SIZE,
     onSelect: (item: CountryData) -> Unit,
     context: Context,
-    dialogStatus: Boolean,
     filteredCountryList: ImmutableList<CountryData>,
     onDismissRequest: () -> Unit,
 ) {
-    var searchValue by remember { mutableStateOf("") }
-    if (!dialogStatus) searchValue = ""
+    var searchValue by rememberSaveable { mutableStateOf("") }
 
     Dialog(
         onDismissRequest = onDismissRequest,
         content = {
             Surface(
-                color = MaterialTheme.colors.onSurface,
+                color = MaterialTheme.colors.surface,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clip(RoundedCornerShape(25.dp)),
+                    .clip(dialogClipShape),
             ) {
-                Scaffold { scaffold ->
-                    scaffold.calculateBottomPadding()
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        SearchTextField(
-                            value = searchValue,
-                            onValueChange = { searchValue = it },
-                            textColor = MaterialTheme.colors.onSurface,
-                            fontSize = 16.sp,
-                            leadingIcon = {
-                                Icon(
-                                    imageVector = Icons.Filled.Search,
-                                    contentDescription = "Search",
-                                    tint = MaterialTheme.colors.onSurface,
-                                    modifier = Modifier.padding(horizontal = 3.dp),
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Text(
+                            text = "Choose your country",
+                            style = MaterialTheme.typography.h6,
+                            color = MaterialTheme.colors.onSurface,
+                        )
+                        Spacer(modifier = Modifier.weight(1f))
+                        IconButton(
+                            onClick = { onDismissRequest() },
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Clear,
+                                contentDescription = "Close",
+                                tint = MaterialTheme.colors.onSurface,
+                            )
+                        }
+                    }
+                    SearchTextField(
+                        value = searchValue,
+                        onValueChange = { searchValue = it },
+                        textColor = MaterialTheme.colors.onSurface,
+                        fontSize = 16.sp,
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Filled.Search,
+                                contentDescription = "Search",
+                                tint = MaterialTheme.colors.onSurface,
+                                modifier = Modifier.padding(horizontal = 3.dp),
+                            )
+                        },
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(50))
+                            .height(40.dp),
+                    )
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    LazyColumn {
+                        items(
+                            if (searchValue.isEmpty()) {
+                                filteredCountryList
+                            } else {
+                                filteredCountryList.searchCountry(
+                                    searchValue,
+                                    context,
                                 )
                             },
-                            modifier = Modifier
-                                .clip(RoundedCornerShape(50))
-                                .height(40.dp),
-                        )
-                        Spacer(modifier = Modifier.height(10.dp))
-
-                        LazyColumn {
-                            items(
-                                if (searchValue.isEmpty()) {
-                                    filteredCountryList
-                                } else {
-                                    filteredCountryList.searchCountry(
-                                        searchValue,
-                                        context,
-                                    )
-                                },
-                            ) { countryItem ->
-                                Row(
-                                    Modifier
-                                        .padding(18.dp)
-                                        .fillMaxWidth()
-                                        .clickable(onClick = { onSelect(countryItem) }),
-                                    horizontalArrangement = Arrangement.Start,
-                                    verticalAlignment = Alignment.CenterVertically,
-                                ) {
-                                    Text(
-                                        text = countryCodeToEmojiFlag(countryItem.countryCode) +
-                                            stringResource(
-                                                id = countryNames.getOrDefault(
-                                                    countryItem.countryCode.lowercase(),
-                                                    R.string.unknown,
-                                                ),
+                        ) { countryItem ->
+                            Row(
+                                Modifier
+                                    .padding(horizontal = rowPadding, vertical = rowPadding * 1.1f)
+                                    .fillMaxWidth()
+                                    .clickable(onClick = { onSelect(countryItem) }),
+                                horizontalArrangement = Arrangement.Start,
+                                verticalAlignment = Alignment.CenterVertically,
+                            ) {
+                                Text(
+                                    text = countryCodeToEmojiFlag(countryItem.countryCode) + "  " +
+                                        stringResource(
+                                            id = countryNames.getOrDefault(
+                                                countryItem.countryCode.lowercase(),
+                                                R.string.unknown,
                                             ),
-                                        modifier = Modifier.padding(horizontal = 18.dp),
-                                        fontSize = 14.sp,
-                                        fontFamily = FontFamily.Serif,
-                                        overflow = TextOverflow.Ellipsis,
-                                    )
-                                }
+                                        ),
+                                    color = MaterialTheme.colors.onSurface,
+                                    fontSize = rowFontSize,
+                                    fontFamily = FontFamily.SansSerif,
+                                    overflow = TextOverflow.Ellipsis,
+                                )
                             }
+                            Divider()
                         }
                     }
                 }
@@ -142,7 +168,6 @@ private fun SearchTextField(
     modifier: Modifier = Modifier,
     textColor: Color = Color.Black,
     leadingIcon: (@Composable () -> Unit)? = null,
-    trailingIcon: (@Composable () -> Unit)? = null,
     hint: String = stringResource(id = R.string.search),
     fontSize: TextUnit = MaterialTheme.typography.body2.fontSize,
 ) {
@@ -160,23 +185,21 @@ private fun SearchTextField(
         ),
         decorationBox = { innerTextField ->
             Row(
-                modifier,
+                Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically,
             ) {
                 if (leadingIcon != null) leadingIcon()
-                Box(Modifier.weight(1f)) {
-                    if (value.isEmpty()) {
-                        Text(
-                            hint,
-                            style = LocalTextStyle.current.copy(
-                                color = textColor,
-                                fontSize = fontSize,
-                            ),
-                        )
-                    }
-                    innerTextField()
+                if (value.isEmpty()) {
+                    Text(
+                        text = hint,
+                        maxLines = 1,
+                        style = LocalTextStyle.current.copy(
+                            color = textColor.copy(alpha = 0.5f),
+                            fontSize = fontSize,
+                        ),
+                    )
                 }
-                if (trailingIcon != null) trailingIcon()
+                innerTextField()
             }
         },
     )
@@ -188,7 +211,6 @@ fun CountryDialogPreview() {
     CountryDialog(
         onSelect = {},
         context = LocalContext.current,
-        dialogStatus = true,
         filteredCountryList = getLibCountries.toImmutableList(),
         onDismissRequest = {},
     )
--- ccp/src/main/java/com/togitech/ccp/component/TogiCodePicker.kt
+++ ccp/src/main/java/com/togitech/ccp/component/TogiCodePicker.kt
@@ -1,30 +1,16 @@
 package com.togitech.ccp.component
 
-import android.content.Context
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Icon
-import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -32,29 +18,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
 import com.togitech.ccp.R
 import com.togitech.ccp.data.CountryData
 import com.togitech.ccp.data.utils.countryCodeToEmojiFlag
 import com.togitech.ccp.data.utils.countryNames
 import com.togitech.ccp.data.utils.getLibCountries
 import com.togitech.ccp.data.utils.unitedStates
-import com.togitech.ccp.utils.searchCountry
 import com.togitech.ccp.utils.sortedByLocalizedName
-import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.ImmutableSet
 import kotlinx.collections.immutable.toImmutableList
 
@@ -68,12 +46,11 @@ fun TogiCodeDialog(
     showCountryCode: Boolean = true,
     showFlag: Boolean = true,
     showCountryName: Boolean = false,
+    textColor: Color = MaterialTheme.colors.onSurface,
 ) {
     val context = LocalContext.current
 
-    var isPickCountry by remember {
-        mutableStateOf(defaultSelectedCountry)
-    }
+    var country by remember { mutableStateOf(defaultSelectedCountry) }
     var isOpenDialog by remember { mutableStateOf(false) }
     val interactionSource = remember { MutableInteractionSource() }
 
@@ -87,44 +64,13 @@ fun TogiCodeDialog(
                 isOpenDialog = true
             },
     ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            if (showCountryCode || showFlag) {
-                Text(
-                    text = if (showFlag) {
-                        countryCodeToEmojiFlag(isPickCountry.countryCode)
-                    } else {
-                        ""
-                    } + if (showCountryCode) {
-                        isPickCountry.countryPhoneCode
-                    } else {
-                        ""
-                    },
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(start = 6.dp),
-                    fontSize = 18.sp,
-                    color = MaterialTheme.colors.onSurface,
-                )
-                Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
-            }
-            if (showCountryName) {
-                Text(
-                    text = stringResource(
-                        id = countryNames.getOrDefault(
-                            isPickCountry.countryCode.lowercase(),
-                            R.string.unknown,
-                        ),
-                    ),
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(start = 6.dp),
-                    fontSize = 18.sp,
-                    color = MaterialTheme.colors.onSurface,
-                )
-                Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
-            }
-        }
+        CodePickerRow(
+            showCountryCode = showCountryCode,
+            showFlag = showFlag,
+            isPickCountry = country,
+            textColor = textColor,
+            showCountryName = showCountryName,
+        )
 
         if (isOpenDialog) {
             val filteredCountryList = getLibCountries.sortedByLocalizedName(context)
@@ -136,10 +82,9 @@ fun TogiCodeDialog(
             CountryDialog(
                 onDismissRequest = { isOpenDialog = false },
                 context = context,
-                dialogStatus = isOpenDialog,
                 onSelect = { countryItem ->
                     onCountryChange(countryItem)
-                    isPickCountry = countryItem
+                    country = countryItem
                     isOpenDialog = false
                 },
                 filteredCountryList = filteredCountryList.toImmutableList(),
@@ -147,6 +92,59 @@ fun TogiCodeDialog(
         }
     }
 }
+
+@Composable
+private fun CodePickerRow(
+    showCountryCode: Boolean,
+    showFlag: Boolean,
+    isPickCountry: CountryData,
+    textColor: Color,
+    showCountryName: Boolean,
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        if (showCountryCode || showFlag) {
+            Text(
+                text = EmojiCodeText(showFlag, isPickCountry, showCountryCode),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(start = 6.dp),
+                fontSize = 18.sp,
+                color = textColor,
+            )
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = null,
+                tint = textColor,
+            )
+        }
+        if (showCountryName) {
+            Text(
+                text = stringResource(
+                    id = countryNames.getOrDefault(
+                        isPickCountry.countryCode.lowercase(),
+                        R.string.unknown,
+                    ),
+                ),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(start = 6.dp),
+                fontSize = 18.sp,
+                color = MaterialTheme.colors.onSurface,
+            )
+            Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+        }
+    }
+}
+
+@Composable
+private fun EmojiCodeText(
+    showFlag: Boolean,
+    isPickCountry: CountryData,
+    showCountryCode: Boolean,
+) = if (showFlag) { countryCodeToEmojiFlag(isPickCountry.countryCode) } else { "" } +
+    if (showCountryCode && showFlag) { "  " } else { "" } +
+    if (showCountryCode) { isPickCountry.countryPhoneCode } else { "" }
 
 @Preview
 @Composable
